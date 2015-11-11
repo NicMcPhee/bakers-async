bac7536447cd0c47fe0a9d13a9dd40444c079e55
Nic McPhee <mcphee@morris.umn.edu>
2015-11-11 08:22:06 -0600

    Decided I don't think I need threads.
    I did some fiddling, and it looks like go blocks provide parallelism quite nicely on their own, so I need to rethink things.

--- src/bakers_async/core.clj
+++ src/bakers_async/core.clj
@@ -3,6 +3,11 @@
              :exclude [map into reduce merge partition partition-by take]])
   (:gen-class))
 
+(defn fib [n]
+  (if (< n 2)
+    n
+    (+ (fib (- n 1)) (fib (- n 2)))))
+
 (defrecord Customer
   [id n fib-of-n])
 
@@ -11,8 +16,10 @@
   (reset! (:fib-of-n customer) value))
 
 (defn add-single-customer-to-channel [customer-id customers-channel]
-  (Thread/sleep (rand-int 5000))
-  (let [customer (->Customer customer-id (rand-int 30) (atom nil))]
+  ; (Thread/sleep (rand-int 1000))
+  (let [n (+ 30 (rand-int 10))
+        customer (->Customer customer-id n (atom (fib n)))]
+    (println (str "Constructured customer: " customer))
     (add-watch (:fib-of-n customer) customer-id
                (fn [key atom old-value new-value]
                  (println (str "Customer " key " changed from " old-value " to " new-value "."))))
@@ -22,6 +29,7 @@
 (defn add-customers-to-channel
   [num-customers customers-channel]
   (doseq [customer-id (range num-customers)]
+    (println (str "Adding customer " customer-id))
     (add-single-customer-to-channel customer-id customers-channel))
   (close! customers-channel))
 
@@ -36,3 +44,14 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+; I'm not sure I need the thread thing. The following
+; pegs all the cores quite nicely, which suggests that
+; go blocks provide parallelism.
+
+(def hi-chan (chan))
+(doseq [n (range 1000)]
+  (go (>! hi-chan (str "hi " (fib (rem n 40))))))
+; Then wait a while for everything to compute
+(close! hi-chan)
+(<!! (async/into [] hi-chan))
